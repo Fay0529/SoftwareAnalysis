d67263adbc59376a233c6df16e6708f6c217adb1
ChenXiaofei <nicholasc@qq.com>
2019-11-26 01:12:45 +0800

    MF1933010

--- White-Box-Testing-/src/main/java/com/test/jacoco/HelloWorld.java
+++ White-Box-Testing-/src/main/java/com/test/jacoco/HelloWorld.java
@@ -1,5 +1,10 @@
 package com.test.jacoco;
 
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
 import javax.imageio.event.IIOReadWarningListener;
 
 
@@ -50,7 +55,18 @@ public class HelloWorld {
     	 * TODO: You need to complete this method to determine whether  a
     	 * triangle is formed or not when given the input edge a, b and c.
     	 */
-    	return true;
+		if(a<=0||b<=0||c<=0){
+			// if there is a zero or negative number
+			return false;
+		}
+		int[] tmp = {a, b, c};
+        Arrays.sort(tmp);
+        if (tmp[0] + tmp[1] > tmp[2]) {
+            return true;
+        }
+
+        return false;
+
     }
     public boolean isBirthday(int year, int month, int day) {
     	/**
@@ -58,8 +74,27 @@ public class HelloWorld {
     	 * legitimate date of birth between 1990/01/01 and 2019/10/01 is 
     	 * formed or not when given the input year, month and day.
     	 */
-    	return true;
-    	
+		
+        // 如果日期不合法，则抛异常
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd",Locale.ENGLISH);
+			String strDate = String.format("%04d-%02d-%02d", year,month,day);
+			sdf.setLenient(false);
+			Date date = sdf.parse(strDate);
+			// 截取当前时间年月日 转成整型
+			long  tempDate=new Date(year - 1990, month - 1, 1).getTime(); 
+			// 截取开始时间年月日 转成整型
+			long  tempDateBegin=new Date(0, 0, 1).getTime();
+			// 截取结束时间年月日   转成整型
+			long  tempDateEnd=new Date(2019 - 1990, 9, 1).getTime();
+			if ((tempDate >= tempDateBegin && tempDate <= tempDateEnd)) {  
+				return true;  
+			} else {  
+				return false;  
+			}  
+		} catch (Exception ex){
+			return false;
+		}
     }
     public Double miniCalculator(double a, double b, char op) {
     	/**
@@ -67,8 +102,24 @@ public class HelloWorld {
     	 * can calculate the formula: "a op b", the op here can be four basic  
     	 * operation: "+","-","*","/". 
     	 */
-    	double result = 0.0;
+		double result = 0.0;
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            default:
+                break;
+        }
     	return result;
-    }
+	}
     
 }
--- White-Box-Testing-/src/test/java/com/test/jacoco/test/HelloWorldTest.java
+++ White-Box-Testing-/src/test/java/com/test/jacoco/test/HelloWorldTest.java
@@ -30,10 +30,76 @@ public class HelloWorldTest {
     	 * TODO：finish the test function
     	 */
     	HelloWorld hw = new HelloWorld();
-    	int a = hw.Method3(6, -1, 1);
-    	
+    	hw.Method3(6, -1, 1);
+        hw.Method3(1, -1, 1);
+        hw.Method3(1, -1, -3);
+        hw.Method3(6, 0, 1);
+        hw.Method3(1, -1, -1);
+        hw.Method3(5, -1, -3);
     }
     /**
      * TODO: add the test function of other methods in HelloWorld.java
      */
+    @Test
+    public void testMethod4() {
+        /**
+         * TODO：finish the test function
+         */
+        HelloWorld hw = new HelloWorld();
+        hw.Method4(5, 5, 1, 1, 1);
+        try {
+            hw.Method4(5, 1, 1, 2, 1);
+        }
+        catch (Exception e) {
+        hw.Method4(0, -1, 1, 1, 1);
+        }
+        hw.Method4(5, 1, 1, 1, 1);
+        try {
+            hw.Method4(4, 1, 1, 1, 1);
+        }
+        catch (Exception e) {
+        }
+    }
+    @Test
+    public void testbug() {
+        HelloWorld hw = new HelloWorld();
+        hw.bug(5);
+        hw.bug(4);
+    }
+
+    @Test
+    public void testisTriangle() {
+        /**
+         * TODO：finish the test function
+         */
+        HelloWorld hw = new HelloWorld();
+        hw.isTriangle(6, 3, 3);
+        hw.isTriangle(13, 9, 5);
+        hw.isTriangle(1, 1, 0);
+        hw.isTriangle(0, 1, 1);
+        hw.isTriangle(1, 0, 1);
+        hw.isTriangle(0, 0, 1);
+        hw.isTriangle(0, 1, 0);
+        hw.isTriangle(1, 0, 0);
+        hw.isTriangle(0, 0, 0);
+    }
+
+    @Test
+    public void testisBirthday() {
+        HelloWorld hw = new HelloWorld();
+        boolean isBirthday = hw.isBirthday(1989, 1, 1);
+        boolean isBirthday2 = hw.isBirthday(2000, 1, 100);
+        boolean isBirthday3 = hw.isBirthday(2000, 1, 1);
+        boolean isBirthday4 = hw.isBirthday(2020, 1, 1);
+    }
+
+    @Test
+    public void testminiCalculator() {
+        HelloWorld hw = new HelloWorld();
+        hw.miniCalculator(6, 1, '+');
+        hw.miniCalculator(6, 1, '-');
+        hw.miniCalculator(6, 1, '*');
+        hw.miniCalculator(6, 1, '/');
+        hw.miniCalculator(6, 0, '1');
+    }
 }
